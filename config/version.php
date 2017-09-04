d6b5276598fce9a40f14faf40a215e5d8106e91e
nic86 <n.eva@simplesolutions.it>
2017-09-04 16:42:47 +0200

    revert config roundcube

--- config/version.php
+++ config/version.php
@@ -3,7 +3,7 @@ return [
 	'appVersion' => '1.0.000',
 	'patchVersion' => '2017.09.04',
 	'lib_mPDF' => '0.0.2',
-	'lib_roundcube' => '0.0.32',
+	'lib_roundcube' => '0.0.33',
 	'lib_PHPExcel' => '0.0.0',
 	'lib_AJAXChat' => '0.0.2',
 	'lib_gantt' => '0.0.0',
