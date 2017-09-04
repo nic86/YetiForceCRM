962cf0e954d79027ff8993d11933ac51f0f23252
nic86 <n.eva@simplesolutions.it>
2017-09-04 15:29:37 +0200

    new background color

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'appVersion' => '4.0.160',
-	'patchVersion' => '2017.04.03',
+	'appVersion' => '1.0.000',
+	'patchVersion' => '2017.09.04',
 	'lib_mPDF' => '0.0.2',
 	'lib_roundcube' => '0.0.32',
 	'lib_PHPExcel' => '0.0.0',
