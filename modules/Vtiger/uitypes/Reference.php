a90d187ce5aec217b11ddc2ece1f87694e7a7443
nic86 <n.eva@simplesolutions.it>
2017-10-12 12:45:20 +0200

    improved documents download with field 1:M

--- modules/Vtiger/uitypes/Reference.php
+++ modules/Vtiger/uitypes/Reference.php
@@ -61,6 +61,15 @@ class Vtiger_Reference_UIType extends Vtiger_Base_UIType
 				return $name;
 			}
 			$name = vtlib\Functions::textLength($name, vglobal('href_max_length'));
+			if($referenceModuleName === 'Documents')
+			{
+				$documentRecordModel = Documents_Record_Model::getInstanceById($value);
+
+				$linkValue ="<a class='btn btn-default btn-sm popoverTooltip' href='".$documentRecordModel->getDownloadFileURL()."' data-docfilter='1' data-content='Apri Documento'><span class='glyphicon glyphicon-open-file' aria-hidden='true'></span> Apri</a>&nbsp;&nbsp;";
+				$linkValue .= "<a class='moduleColor_$referenceModuleName' href='index.php?module=$referenceModuleName&view=" . $referenceModule->getDetailViewName() . "&record=$value' title='" . App\Language::translate($referenceModuleName, $referenceModuleName) . "'>$name</a>";
+				return $linkValue;
+			}
+
 			$linkValue = "<a class='moduleColor_$referenceModuleName' href='index.php?module=$referenceModuleName&view=" . $referenceModule->getDetailViewName() . "&record=$value' title='" . App\Language::translate($referenceModuleName, $referenceModuleName) . "'>$name</a>";
 			return $linkValue;
 		}
@@ -86,6 +95,15 @@ class Vtiger_Reference_UIType extends Vtiger_Base_UIType
 				return $name;
 			}
 			$name = vtlib\Functions::textLength($name, $this->get('field')->get('maxlengthtext'));
+			if($referenceModuleName === 'Documents')
+			{
+				$documentRecordModel = Documents_Record_Model::getInstanceById($value);
+
+				$linkValue ="<a class='btn btn-default btn-sm popoverTooltip' href='".$documentRecordModel->getDownloadFileURL()."' data-docfilter='1' data-content='Apri Documento'><span class='glyphicon glyphicon-open-file' aria-hidden='true'></span> Apri</a>&nbsp;&nbsp;";
+				$linkValue .= $name;
+				return $linkValue;
+			}
+
 			$title=App\Language::translate($referenceModuleName, $referenceModuleName);
 			$linkValue ="<a class='btn btn-default btn-sm' role='button' href='index.php?module={$referenceModuleName}&view={$referenceModule->getDetailViewName()}&record={$value}' title='{$title}'><span class='glyphicon glyphicon-link' aria-hidden='true'></span></a>&nbsp;&nbsp;<span class='moduleColor_{$referenceModuleName}'>{$name}</span>";
 			return $linkValue;
